927b97c4cb2770183031547f680f75aeeb166791
realTronsi <63553774+realTronsi@users.noreply.github.com>
2020-05-08 17:37:47 -0400

    Create main.js

---
+++ main.js
@@ -0,0 +1,14 @@
+var PlayerStats = {
+
+  HP:100,
+  Equppied:"Pistol",
+  Speed: 10,
+  Level: 1,
+  Name: "NA",
+};
+
+function update(){
+  document.write("hello");
+}
+
+setInterval(update, 15);
