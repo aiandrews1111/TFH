4e67f4f36d3594b6b68437affcab0ca00a047918
realTronsi <63553774+realTronsi@users.noreply.github.com>
2020-05-09 16:02:16 -0400

    Bug fix

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
-var canvas = document.getElementById("canvas");
-var ctx = canvas.getContext("2d");
+var canvas = document.getElementById('canvas');
+var ctx = canvas.getContext('2d');
 
 canvas.width = canvas.height = 300;
 
