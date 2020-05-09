bb993bb0e05a23e9314be2b95aaaece4b5b285e1
realTronsi <63553774+realTronsi@users.noreply.github.com>
2020-05-09 13:29:54 -0400

    Added arrow key support and bug fix

--- main.js
+++ main.js
@@ -13,24 +13,24 @@ var PlayerStats = {
   Name: "NA",
 };
 
-document.addEventListener('keydown', event => {
-  if (event.keyCode === 87) {
+ddocument.addEventListener('keydown', event => {
+  if (event.keyCode === 87 || event.keyCode === 38) {
     playerY = playerY - 2;
-    //W key
-  } else if (event.keyCode === 65) {
+    //W key or Up key
+  } else if (event.keyCode === 65 || event.keyCode === 37) {
     playerX = playerX - 2;
-    //A Key
-  } else if (event.keyCode === 63) {
+    //A Key or Left key
+  } else if (event.keyCode === 83 || event.keyCode === 40) {
     playerY = playerY + 2;
-    //S Key
-  } else if (event.keyCode === 68) {
+    //S Key or Down key
+  } else if (event.keyCode === 68 || event.keyCode === 39) {
     playerX = playerX + 2;
-    //D Key
+    //D Key or Right key
   }
 });
                           
 function updatePlayer(x, y){
-  ctx.fillStyle = "rpg(204, 0, 0)";
+  ctx.fillStyle = "rbg(204, 0, 0)";
   ctx.fillRect(x, y, 50, 50);
 }
 
