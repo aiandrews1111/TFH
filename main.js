b29e906a1997822b1eca36edfadcc8237f8ed6cc
realTronsi <63553774+realTronsi@users.noreply.github.com>
2020-05-10 10:21:14 -0400

    Bug fix

--- main.js
+++ main.js
@@ -29,8 +29,8 @@ function update() {
     requestAnimationFrame(update);
     
     if (bulletDirX != 0){
-    bulletx += (bulletSpeed/sqrt(Math.pow(bulletDirX, 2) + Math.pow(bulletDirY, 2)))*bulletDirX
-    bullety += (bulletSpeed/sqrt(Math.pow(bulletDirX, 2) + Math.pow(bulletDirY, 2)))*bulletDirY
+    bulletx += (bulletSpeed/Math.sqrt(Math.pow(bulletDirX, 2) + Math.pow(bulletDirY, 2)))*bulletDirX
+    bullety += (bulletSpeed/Math.sqrt(Math.pow(bulletDirX, 2) + Math.pow(bulletDirY, 2)))*bulletDirY
     ctx.arc(bulletx, bullety, 2, 0, Math.PI * 2);
     }
     //temporary location
