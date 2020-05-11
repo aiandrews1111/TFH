0b78f316b0dd3c55bf7dbaf07115e39e60169d03
haha0201 <47706454+haha0201@users.noreply.github.com>
2020-05-10 23:17:07 -0400

    Enemies are all fixed!
    Here we go.

--- main.js
+++ main.js
@@ -97,8 +97,9 @@ Enemy.prototype.draw = function() {
          var disty = bullets[i].y - this.y;
          var dist = Math.pow(Math.pow(distx, 2) + Math.pow(disty, 2), 0.5);
          console.log(dist);
-         if (dist<this.size/2 + 1){
+         if (dist<this.size + 2){
              this.hp -= bulletDamage;
+             bullets[i].delete = 1;
          }
     }
     
